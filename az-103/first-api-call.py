9131d9c502b5730a19e2f18ab5ca11a472d5c63e
Jason Lam <lwh.jlin@gmail.com>
2026-09-05 01:13:29 -0400

    updated grok details

--- az-103/first-api-call.py
+++ az-103/first-api-call.py
@@ -1,9 +1,9 @@
 from openai import OpenAI
+import os
 
-endpoint = 
-deployment_name = 
-api_key = 
-
+endpoint = "https://az-103-study-jl1.services.ai.azure.com/openai/v1/chat/completions"
+deployment_name = "grok-4.3"
+api_key = os.getenv("API_KEY")
 client=openAI(
     base_rule=endpoint,
     api_key=api_key
